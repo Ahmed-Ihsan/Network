5bdd12a9de44403bed35144701b4a6ebafd987f1
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2020-12-04 10:58:50 -0800

    Update network.py

--- network.py
+++ network.py
@@ -1,8 +1,42 @@
 import networkx as nx
-G=nx.Graph()
+import pydot
+import os
 
-print(G.nodes())
-print(G.edges())
+G=nx.DiGraph()
 
-print(type(G.nodes()))
-print(type(G.edges()))
+G.add_node('A',pos="0,1!" )
+G.add_node('B',pos="0,-1!" )
+G.add_node('C',pos="1,1!" )
+G.add_node('D',pos="1,-1!" )
+G.add_node('E',pos="2,1!" )
+G.add_node('F',pos="2,-1!" )
+G.add_node('G',pos="3,0!" )
+
+pos_node=[5.4,10.15,5.10,7.8,3.17,3.4,1.4,5.6,8.8,1.14,2.5]
+
+G.add_edge('A','D',weigth=pos_node[0],label=f'w:{pos_node[0]}')
+G.add_edge('A','C',weigth=pos_node[1],label=f'w:{pos_node[1]}')
+G.add_edge('B','D',weigth=pos_node[2],label=f'w:{pos_node[2]}')
+G.add_edge('B','C',weigth=pos_node[3],label=f'w:{pos_node[3]}')
+G.add_edge('C','E',weigth=pos_node[4],label=f'w:{pos_node[4]}')
+G.add_edge('C','F',weigth=pos_node[5],label=f'w:{pos_node[5]}')
+G.add_edge('D','C',weigth=pos_node[6],label=f'w:{pos_node[6]}')
+G.add_edge('D','E',weigth=pos_node[7],label=f'w:{pos_node[7]}')
+G.add_edge('D','F',weigth=pos_node[8],label=f'w:{pos_node[8]}')
+G.add_edge('E','G',weigth=pos_node[9],label=f'w:{pos_node[9]}')
+G.add_edge('G','F',weigth=pos_node[10],label=f'w:{pos_node[10]}')
+
+p = nx.shortest_path(G, source='B', weight='weight')
+
+print(p['E'])
+
+M=nx.MultiDiGraph(G)
+
+M.add_edge(p['E'][-1],p['E'][-2],color='red')
+
+
+filename = "nx_sp"
+nx.drawing.nx_pydot.write_dot(M, filename + ".dot")
+
+os.system("neato -T png " + filename + ".dot -o " + filename + ".png")
+os.system("dot -T png " + filename + ".dot -o " + filename + ".png")
