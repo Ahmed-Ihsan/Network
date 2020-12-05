75bfd8b9f2950ec4dab9bdc3df1cda8d7d261fe9
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2020-12-05 08:43:40 -0800

    Update network.py

--- network.py
+++ network.py
@@ -1,16 +1,19 @@
 import networkx as nx
 import pydot
+import graphviz
 import os
 
 G=nx.DiGraph()
+h=graphviz.Graph()
 
+h.engine='neato'
 G.add_node('A',pos="0,1!" )
 G.add_node('B',pos="0,-1!" )
-G.add_node('C',pos="1,1!" )
-G.add_node('D',pos="1,-1!" )
-G.add_node('E',pos="2,1!" )
-G.add_node('F',pos="2,-1!" )
-G.add_node('G',pos="3,0!" )
+G.add_node('C',pos="2,1!" )
+G.add_node('D',pos="2,-1!" )
+G.add_node('E',pos="4,1!" )
+G.add_node('F',pos="4,-1!" )
+G.add_node('G',pos="6,0!" )
 
 pos_node=[5.4,10.15,5.10,7.8,3.17,3.4,1.4,5.6,8.8,1.14,2.5]
 
@@ -28,15 +31,22 @@ G.add_edge('G','F',weigth=pos_node[10],label=f'w:{pos_node[10]}')
 
 p = nx.shortest_path(G, source='B', weight='weight')
 
+print(p['D'])
 print(p['E'])
 
 M=nx.MultiDiGraph(G)
 
+M.add_edge(p['D'][-1],p['D'][-2],color='red')
 M.add_edge(p['E'][-1],p['E'][-2],color='red')
-
 
 filename = "nx_sp"
 nx.drawing.nx_pydot.write_dot(M, filename + ".dot")
 
-os.system("neato -T png " + filename + ".dot -o " + filename + ".png")
-os.system("dot -T png " + filename + ".dot -o " + filename + ".png")
+# in windows graphviz 
+# run neato by dot -Kneato
+os.system("dot -Kneato -T png "+ filename + ".dot -o " + filename + ".png")
+# or dot by dot only
+#os.system("dot -T png " + filename + ".dot -o " + filename + ".png")
+
+
+
